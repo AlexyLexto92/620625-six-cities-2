ee312cd67c4d3247d368b087d79b128f1aa6f56f
lexto92@gmail.com <lexto92@gmail.com>
2019-10-18 19:05:51 +0300

    ввод пропсов и их валидация

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,5 +1,17 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import {HomePage} from './../home page/home_page.jsx';
-export const App = () => {
-  return <HomePage/>;
+export const App = (props) => {
+  debugger
+  const {objectsForRent} = props;
+  return <HomePage
+    objectsForRent={objectsForRent}
+  />;
+};
+
+App.propTypes = {
+  title: PropTypes.string.isRequired,
+  coast: PropTypes.number.isRequired,
+  type: PropTypes.oneOf([`Private room`, `Apartment`]),
+  isPremium: PropTypes.bool,
 };
--- src/index.js
+++ src/index.js
@@ -3,6 +3,42 @@ import ReactDOM from 'react-dom';
 import {App} from './components/app/app.jsx';
 
 const init = () => {
-  ReactDOM.render(<App/>, document.querySelector(`#root`));
+
+  const objectsForRent = [
+    {
+      title: `Beautiful & luxurious apartment at great location`,
+      coast: 120,
+      type: `Apartment`,
+      isPremium: true,
+    },
+    {
+      title: `Wood and stone place`,
+      coast: 80,
+      type: `Private room`,
+      isPremium: false,
+    },
+    {
+      title: `Canal View Prinsengracht`,
+      coast: 132,
+      type: `Apartment`,
+      isPremium: false,
+    },
+    {
+      title: `Nice, cozy, warm big bed apartment`,
+      coast: 180,
+      type: `Apartment`,
+      isPremium: false,
+    },
+    {
+      title: `Wood and stone place`,
+      coast: 80,
+      type: `Private room`,
+      isPremium: true,
+    }
+  ];
+
+  ReactDOM.render(<App
+    objectsForRent={objectsForRent}
+  />, document.querySelector(`#root`));
 };
 init();
