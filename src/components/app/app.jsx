d22088d519f9c667a5e7621cb62017878d36290e
lexto92@gmail.com <lexto92@gmail.com>
2019-10-19 19:56:33 +0300

    исправления по замечаниям

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,20 +1,18 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import {HomePage} from './../home page/home_page.jsx';
-export const App = ({rents}) => {
-  return <HomePage
+import { HomePage } from './../home page/home_page.jsx';
+export const App = ({ rents }) => (<HomePage
   rents={rents}
-  />;
-};
+/>);
 
 App.propTypes = {
   rents: PropTypes.arrayOf(
-      PropTypes.shape({
-        title: PropTypes.string,
-        coast: PropTypes.number,
-        type: PropTypes.string,
-        isPremium: PropTypes.bool,
-        img: PropTypes.string
-      })
+    PropTypes.shape({
+      title: PropTypes.string,
+      coast: PropTypes.number,
+      type: PropTypes.string,
+      isPremium: PropTypes.bool,
+      img: PropTypes.string
+    })
   )
 };
